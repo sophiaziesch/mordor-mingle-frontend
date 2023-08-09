fe66515b1bc03f4063c345077250c6d2a37481b2
Alcaraz <jaime.mediano@gmail.com>
2023-08-09 11:59:18 +0200

    IsPrivate working fine

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,24 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/Auth.context";
 import { useNavigate } from "react-router-dom";
 
+// function IsPrivate({ children }) {
+//   //grabbing information from the context (from the frig)
+//   const { isLoading, isLoggedIn } = useContext(AuthContext);
+//   const navigate = useNavigate();
+
+//   //If the page is still loading, then return a p tag until the data arrives
+//   if (isLoading) {
+//     return <p>Loading...</p>;
+//   }
+//   //If the data has arrived and the user is still not logged IN, then redirect to the login page
+//   if (!isLoggedIn) {
+//     navigate("/login");
+//     return null;
+//   }
+
+//   // ELse... return the child component.
+//   //Everything was ok
+
+//   return <div>{children}</div>;
+// }
+
 function IsPrivate({ children }) {
-  //grabbing information from the context (from the frig)
   const { isLoading, isLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  //If the page is still loading, then return a p tag until the data arrives
+  useEffect(() => {
+    if (!isLoading && !isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoading, isLoggedIn, navigate]);
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
-  //If the data has arrived and the user is still not logged IN, then redirect to the login page
-  if (!isLoggedIn) {
-    navigate("/login");
-    return null;
-  }
-
-  // ELse... return the child component.
-  //Everything was ok
 
   return <div>{children}</div>;
 }
