2428a9abc37eb77802095148e9b91499771a8f37
Sophia Ziesch <s.ziesch@posteo.de>
2023-08-10 12:49:05 +0200

    logo fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,36 +2,37 @@ import { Link } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/Auth.context";
+import logo from "../assets/Logo-white.png";
 
 const NavBar = () => {
-  const { isLoggedIn, user } = useContext(AuthContext);
-  return (
-    <>
-      <nav className="navbar">
-        <img className="logo" src="../src/assets/Logo-white.png" alt="logo" />
-        <ul>
-          <div className="links">
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/events">Events</Link>
-            </li>
-            <li>
-              <Link to="/events/new">New Event</Link>
-            </li>
-            <li>{isLoggedIn && <Link to={`/${user._id}`}>Profile</Link>}</li>
-            <li>
-              <LogoutButton />
-            </li>
-          </div>
-        </ul>
-      </nav>
-    </>
-  );
+	const { isLoggedIn, user } = useContext(AuthContext);
+	return (
+		<>
+			<nav className="navbar">
+				<img className="logo" src={logo} alt="logo" />
+				<ul>
+					<div className="links">
+						<li>
+							<Link to="/signup">Signup</Link>
+						</li>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/events">Events</Link>
+						</li>
+						<li>
+							<Link to="/events/new">New Event</Link>
+						</li>
+						<li>{isLoggedIn && <Link to={`/${user._id}`}>Profile</Link>}</li>
+						<li>
+							<LogoutButton />
+						</li>
+					</div>
+				</ul>
+			</nav>
+		</>
+	);
 };
 
 export default NavBar;
