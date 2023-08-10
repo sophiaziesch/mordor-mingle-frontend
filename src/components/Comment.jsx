d2682c9473e464d0f85c0cce65c43cb3052c166c
Sophia Ziesch <s.ziesch@posteo.de>
2023-08-10 08:59:47 +0200

    debugging comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,8 +11,8 @@ function Comment({ comment }) {
 
 	return (
 		<div className="comment">
-			<ProfileImage image={comment.user.image} />
-			<h3>{comment.user.username}</h3>
+			<ProfileImage image={comment.userId.image} />
+			<h3>{comment.userId.username}</h3>
 			<h4>{formattedDate}</h4>
 			<p>{comment.text}</p>
 		</div>
